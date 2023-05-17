97217e2e1202084671f02268c40be66844c3a30a
Gameunkulus <bugr24-8@mail.ru>
2023-05-17 12:04:11 +0300

    programm-2 redacted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int rubles = 13676;
-        int miles = 20;
-        int bonuses = rubles/miles;
-        System.out.println(bonuses);
+        int account = 100;
+        System.out.println(bonusAdds(300,account));
+        System.out.println(bonusAdds(1100,account));
+    }
+
+    public static int bonusAdds(int x, int y){
+
+        if (x > 1000) {
+            return y + x + (x/100);
+        }
+        else{
+            return y + x;
+        }
     }
 }
