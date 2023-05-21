559cac7d3fb2101b769da19987145f75fdf21331
Gameunkulus <bugr24-8@mail.ru>
2023-05-21 12:56:35 +0300

    programm2 redacted 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 100;
-        System.out.println(bonusAdds(300, account));
-        System.out.println(bonusAdds(1100, account));
+        System.out.println(bonusMiles(300, account));
+        System.out.println(bonusMiles(1100, account));
     }
 
-    public static int bonusAdds(int miles, int account) {
+    public static int bonusMiles(int miles, int account) {
 
         if (miles > 1000) {
             return account + miles + (miles / 100);
