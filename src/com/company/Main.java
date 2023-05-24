6526e048849682bc2f211b3c0d47a03e22273890
Gameunkulus <bugr24-8@mail.ru>
2023-05-24 17:59:44 +0300

    programm2 redacted 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         int account = 100;
         System.out.println(bonuses(300, account));
         System.out.println(bonuses(1100, account));
     }
 
     public static int bonuses(int miles, int account) {
-
         if (miles > 1000) {
             return account + miles + (miles / 100);
         } else {
